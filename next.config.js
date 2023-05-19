6de0c09036968a21ff9488c68a4c301643bfa490
hamza-jadli <jadli.hamza@gmail.com>
2023-05-19 15:00:37 +0000

    test

--- next.config.js
+++ next.config.js
@@ -4,4 +4,14 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/admin',
+        destination: 'https://dashboard-gcpn.vercel.app/home',
+      },
+    ];
+  },
 }
+
+
