4e34f9653fcfceca0038564bffb4d2b05b0a47f7
hamza-jadli <jadli.hamza@gmail.com>
2023-05-20 16:35:45 +0000

    test4

--- utils/data/products.ts
+++ utils/data/products.ts
@@ -65,64 +65,7 @@ export const products = [
       'https://mgbookvillage.files.wordpress.com/2019/07/51k6isgquql._sx329_bo1204203200_.jpg?w=331',
     ],
   },
-  {
-    id: '6',
-    name: 'T-Shirt Summer Vibes',
-    price: 119.99,
-    discount: 30,
-    quantityAvailable: 2,
-    category: 'T-shirt',
-    currentPrice: 89.99,
-    images: [
-      '/images/products/product-6.jpg',
-    ],
-    punctuation: {
-      countOpinions: 81,
-      punctuation: 4.5,
-      votes: [
-        {
-          value: 1,
-          count: 1
-        },
-        {
-          value: 2,
-          count: 10
-        },
-        {
-          value: 3,
-          count: 10
-        },
-        {
-          value: 4,
-          count: 20
-        },
-        {
-          value: 5,
-          count: 40
-        },
-      ]
-    },
-    reviews: [
-      {
-        name: 'John Doe',
-        avatar: '/images/featured-1.jpg',
-        description: '<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s</p>',
-        punctuation: 2
-      },
-      {
-        name: 'John Doe',
-        avatar: '/images/featured-1.jpg',
-        description: '<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s</p>',
-        punctuation: 4
-      },
-      {
-        name: 'John Doe',
-        avatar: '/images/featured-1.jpg',
-        description: '<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s</p>',
-        punctuation: 5
-      },
-    ]
-  },
+  
 ];
 
 export default products;
