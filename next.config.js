b3460b515981e099ca936c3e4244810292a72c4b
hamza-jadli <jadli.hamza@gmail.com>
2023-05-20 19:08:13 +0000

    test5

--- next.config.js
+++ next.config.js
@@ -8,7 +8,7 @@ module.exports = {
     return [
       {
         source: '/admin/:path*',
-        destination: 'https://dashboard-gcpn.vercel.app/admin/:path*',
+        destination: 'https://dashboard-gcpn.vercel.app/:path*',
       },
     ];
   },
